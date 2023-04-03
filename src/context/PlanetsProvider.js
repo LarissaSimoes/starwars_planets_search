31d82eaf1fd9ed59fa6b723b53be63bf6053cfd1
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-04-03 09:13:48 -0300

    Iniciando projeto, req 1

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,41 +1,31 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import PlanetsContext from './PlanetsContext';
 
-function PlanetProvider({ children }) {
-  const [isLoading, setIsLoading] = useState(false);
+function PlanetsProvider({ children }) {
+  // const [isLoading, setIsLoading] = useState(false);
   const [planets, setPlanets] = useState([]);
-  const [errors, setErrors] = useState(null);
+  // const [errors, setErrors] = useState(null);
 
+  // Alterações
   const fetchPlanets = async () => {
-    try {
-      setIsLoading(true);
-      const data = await fetch('https://swapi.dev/api/planets');
-
-      if (!data.ok) {
-        const newError = await data.json();
-        throw newError.message;
-      }
-
-      const json = await data.json();
-      const planetResults = json.results.forEach((planet) => {
-        delete planet.residents;
-      });
-      setPlanets(planetResults);
-    } catch (e) {
-      setErrors(e);
-      throw new Error(e);
-    } finally {
-      setIsLoading(false);
-    }
+    const data = await fetch('https://swapi.dev/api/planets');
+    const json = await data.json();
+    const planetResults = json.results.map((planet) => {
+      const newPlanets = { ...planet };
+      delete newPlanets.residents;
+      return newPlanets;
+    });
+    setPlanets(planetResults);
   };
 
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
+
   const values = useMemo(() => ({
-    isLoading,
-    errors,
     planets,
-    fetchPlanets,
-  }), [isLoading, errors, planets]);
+  }), [planets]);
 
   return (
     <PlanetsContext.Provider value={ values }>
@@ -44,8 +34,8 @@ function PlanetProvider({ children }) {
   );
 }
 
-export default PlanetProvider;
-
-PlanetProvider.propTypes = {
+PlanetsProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+export default PlanetsProvider;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import PlanetProvider from './context/PlanetsProvider';
+import PlanetsProvider from './context/PlanetsProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <PlanetProvider>
+    <PlanetsProvider>
       <App />
-    </PlanetProvider>,
+    </PlanetsProvider>,
   );
