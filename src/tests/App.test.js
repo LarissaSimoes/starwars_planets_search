642dd801eb030a40846257d81769087975736dd0
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-04-20 14:13:02 -0300

    Testes em andamento

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import PlanetsProvider from '../context/PlanetsProvider';
 import FiltersProvider from '../context/FiltersProvider';
 
 describe('Testando aplicação Star Wars', () => {
-  jest.setTimeout(70000);
-  it('Testando se elementos estão presentes na página', () => {
+  // jest.setTimeout(10000);
+  
+  it('Testando se elementos estão presentes na página', async () => {
     render(    
     <FiltersProvider>
       <PlanetsProvider>
@@ -18,6 +20,15 @@ describe('Testando aplicação Star Wars', () => {
     expect(firstInput).toBeInTheDocument();
     const firstSelect = screen.getByTestId(/column-filter/i);
     expect(firstSelect).toBeInTheDocument();
+    const comparisonFilter = screen.getByTestId(/comparison-filter/i);
+    expect(comparisonFilter).toBeInTheDocument();
+    const valueFilter = screen.getByTestId(/value-filter/i);
+    expect(valueFilter).toBeInTheDocument();
+    const buttonFilter = screen.getByTestId(/button-filter/i);
+    expect(buttonFilter).toBeInTheDocument();
 
+    userEvent.type(screen.getByPlaceholderText('Procurar pelo nome'), 'oo');
+    const tatooine = await screen.findByText(/tatooine/i);
+    expect(tatooine).toBeInTheDocument();
   })
 })
